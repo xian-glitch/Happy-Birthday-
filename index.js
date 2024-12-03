0cdc411c97d8e5336d084a8a3d97631ea1604d27
xian-glitch <xianecong@gmail.com>
2024-12-03 20:17:39 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -5,19 +5,49 @@ const othButton = document.getElementById('other');
 const container = document.getElementById('division');
 const body = document.getElementById('mainbod');
 const next = document.querySelector(".next");
-const end = document.getElementById('the-end')
+const end = document.getElementById('the-end');
+const letter = document.getElementById('letter');
+const oth = document.getElementById('oth');
+const giphy = document.getElementById('giphy-embed');
+const color = document.getElementById('color'); 
+const h = document.getElementById('h');
+
+color.addEventListener("click", () =>{
+    color.style.visibility = ('hidden');
+    giphy.style.display = ('block');
+    
+})
+
+
+
+
 
 function myfunction(){
-    body.style.backgroundColor = ('white');
-    othButton.style.borderColor = ('black');
-    if(othButton.onclick){
-        othButton.style.display = ('none');
-    }else{
-    };    
+    body.style.background =  ('rgb(2,0,36)');
+    body.style.background = ("linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)");
+    body.style.width = ('100%');
+    body.style.height = ('100%');
+    othButton.style.display = ('none')
+    animate.style.display = ('none');
+    letter.style.display = ('block');
+    oth.style.display = ('block');
 }
 
 function myOth(){
     location.reload()
+}
+
+function idk(){
+    letter.style.display = ('none');
+    oth.style.display = ('none');
+    body.style.background = ('none');
+    body.style.background = ('rgb(131,58,180)');
+    body.style.background = ('linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)');
+    color.style.display = ('block');
+    color.style.marginLeft = ('500px');
+    giphy.style.marginLeft = ('500px');
+    h.innerHTML = ("Sorry that I didn't buy you cake irl ,but here's a virtual one!")
+    
 }
 
 form.addEventListener('submit', (e) =>{
